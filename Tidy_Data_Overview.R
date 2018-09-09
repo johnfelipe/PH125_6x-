43a21476e9bb5401df0d43ef2b7cc836ac3677ac
kg4nih <User@Mac-Pro.local>
2018-09-09 17:05:42 -0400

    adding new files

--- Tidy_Data_Overview.R
+++ Tidy_Data_Overview.R
@@ -42,6 +42,8 @@ select(wide_data, '1960':'1967')
 new_tidy_data <- wide_data %>% 
                   gather(year, fertility, -country, convert = TRUE)
 
+head(new_tidy_data)
+
 # just running a plot and saving it to the plot folder
 # I set to working directory to the plot folder. Need to make sure to set the
 # working directory back to the project folder
@@ -51,6 +53,52 @@ new_tidy_data %>%
   geom_point() +
   ggsave("fertility.png")
 
+# now I'm going to use the spread() function to reverse the gather()
+#
 
+new_wide_data <- new_tidy_data %>% 
+                    spread(year, fertility)
 
+head(new_wide_data)
 
+# downloading and reading another data file for processing 
+# to the projects raw_data" folder
+# remember to set the working directory to the raw data folder
+
+path <- system.file("extdata", package = "dslabs")
+list.files(path)
+fullpath <- file.path(path, "life-expectancy-and-fertility-two-countries-example.csv")
+file.copy(fullpath, getwd())
+
+# reading the file to a new tibble
+
+wide_data_2 <- read_csv("life-expectancy-and-fertility-two-countries-example.csv") 
+
+# inspecting the tibble
+
+select(wide_data_2, 1:5)
+
+# columns are variables that combine year and fertility AND year and life expectancy
+# starting to wrangle. 
+# step 1 First, place the column headers into a single column as variables
+# excluding the country
+
+dat <- wide_data_2 %>% 
+        gather(key,value, - country)
+
+head(dat)
+
+# step 2  using the separate() to separte the key into 2 parts
+# note since key is of the form year_life_expentance
+# use extra = "merge" 
+
+dat_2 <- dat %>% 
+  separate(key, c("year", "variable_name"), sep = "_", extra = "merge")
+
+head(dat_2)
+
+# now use the spread() function to make columns for fertility and life_expectancey
+
+new_tidy_data_2 <- dat_2 %>% 
+                    spread(variable_name, value)
+head(new_tidy_data_2)
