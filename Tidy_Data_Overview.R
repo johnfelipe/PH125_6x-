300615702cfa07fda352f66bb093bcbc66d33cc9
kg4nih <User@Mac-Pro.local>
2018-09-11 17:07:29 -0400

    more string conversion practice

--- Tidy_Data_Overview.R
+++ Tidy_Data_Overview.R
@@ -361,3 +361,34 @@ str_match(problems, pattern_w_groups)
 
 pattern_w_groups2 <- "^([4-7])[,\\.\\s](\\d*)$"
 str_match(problems, pattern_w_groups2)
+
+yes <- c("5 feet 7inches", "5 7")
+no <- c("5ft 9 inches", "5 ft 9 inches")
+s <- c(yes, no)
+s
+
+converted <- s %>% 
+  str_replace("feet|foot|ft", "'") %>% 
+  str_replace("inches|in|''|\"", "") %>% 
+  str_replace("^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$", "\\1'\\2")
+
+pattern <- "^[4-7]\\s*'\\s*\\d{1,2}$"
+
+str_detect(converted, pattern)
+str_match(converted,pattern)
+
+converted <- s %>% 
+  str_replace("\\s*|feet|foot|ft", "'") %>% 
+  str_replace("\\s*|inches|in|''|\"", "") %>% 
+  str_replace("^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$", "\\1'\\2") 
+
+str_detect(converted, pattern)
+str_match(converted,pattern)
+
+converted <- s %>% 
+  str_replace("\\s*(feet|foot|ft)\\s*", "'") %>% 
+  str_replace("\\s*(inches|in|''|\")\\s*", "") %>% 
+  str_replace("^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$", "\\1'\\2")
+
+str_detect(converted, pattern)
+str_match(converted,pattern)
